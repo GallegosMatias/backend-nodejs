ec2d213c0fd4d0bed70df0f550fb87b7c5e02fa2
Matias <gallegosmatiasjoel@gmail.com>
2020-02-25 21:38:26 -0300

    Backend terminado

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 var app = express();
 
 // cargar archivos de rutas
+var project_routes = require('./routes/project');
 
 // middlewares
 app.use(bodyParser.urlencoded({extended:false}));
@@ -14,7 +15,7 @@ app.use(bodyParser.json());
 // CORS
 
 // cargar rutas
-app.post('/test', (req,res) => {
+/*app.post('/test', (req,res) => {
   console.log(req.body.name);
   res.status(200).send({
     message: "Hola mundo desde mi api de nodejs"
@@ -25,6 +26,9 @@ app.get('/', (req,res) => {
   res.status(200).send(
     "<h2>Hola mundo desde mi api de nodejs</h2>"
   );
-});
+});*/
+
+app.use('/api', project_routes);
+
 // exportar
 module.exports = app;
